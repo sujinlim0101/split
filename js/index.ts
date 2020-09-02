308329bf38c68847c861d0d57fb6e9e2575ac17d
sujinlim0101 <60640614+sujinlim0101@users.noreply.github.com>
2020-09-02 16:27:14 +0900

    Set Layouts and svgs

---
+++ js/index.ts
@@ -0,0 +1,9 @@
+(() => {
+  let yOffset = 0;
+  let prevScrollHeight = 0;
+  let currentScene = 0;
+  let enterNewScene = false;
+  const left_split_0 = document.querySelector(".beer ");
+  const left_split_1 = document.querySelector(".introduce ");
+  console.log(left_split_0, left_split_1);
+})();
